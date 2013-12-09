067c6ed00b6831a4aa369936ec5a3c655d6da083
Leandro Lovisolo <leandro@leandro.me>
2013-12-08 21:00:23 -0300

    Ignorar usuarios nulos

--- code/ej3a/map.js
+++ code/ej3a/map.js
@@ -1,4 +1,9 @@
 function() {
+	// Ignorar usernames vacíos. Algunas entradas en el cuerpo de datos
+	// no tienen usuarios. Probablemente correspondan a las entradas cuyo
+	// autor aparece como [deleted] en reddit.
+	if(!this.username) return;
+
     emit(this.username, {
     	username: this.username,
     	score:    this.score
